877860b6f22f4bd32e1cd5ce4872d1a4587be622
1ellejade <107277977+1ellejade@users.noreply.github.com>
2023-04-17 14:30:32 -0400

    Update subq_1.py

--- Q1-Part1/subq_1.py
+++ Q1-Part1/subq_1.py
@@ -1,13 +1,8 @@
 from __future__ import print_function
 import sys
-import math
-from operator import add
+
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, year, month, dayofmonth, to_date, regexp_replace
-from pyspark.ml.clustering import KMeans
-from pyspark.ml.evaluation import ClusteringEvaluator
-from pyspark.ml.feature import VectorAssembler
-from pyspark.ml.linalg import Vector
+from pyspark.sql.functions import col
 
 
 if __name__ == "__main__":
@@ -17,7 +12,12 @@ if __name__ == "__main__":
     .getOrCreate()
 
   
-  dataset = spark.read.format("csv").option("inferSchema", "true").option("header", "true").load('/content/parking_sampled.csv') 
+  dataset = spark.read\
+    .format("csv")\
+    .option("inferSchema", "true")\
+    .option("header", "true")\
+    .load(sys.argv[1]) 
+  
   time_df = dataset.select(col('Violation Time'), col('Summons Number'))
   df2=time_df.rdd.map(lambda x: ((str(int(x['Violation Time'][:2])+12) if x['Violation Time'][4]=='P' else x['Violation Time'][:2], x['Summons Number']))).toDF(['Violation Time','Summons Number'])
   df3 = df2.groupBy('Violation Time').count()
