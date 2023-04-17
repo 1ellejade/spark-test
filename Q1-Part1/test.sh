120d7a6dfb8c35058a9b0296f3f71c9e90321056
1ellejade <107277977+1ellejade@users.noreply.github.com>
2023-04-17 14:25:27 -0400

    Update test.sh

--- Q1-Part1/test.sh
+++ Q1-Part1/test.sh
@@ -2,5 +2,5 @@
 source ../env.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /Q1-Part1/input/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /Q1-Part1/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../shot_logs.csv /Q1-Part1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../parking_sampled.csv /Q1-Part1/input/
 /usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./subq_1.py hdfs://$SPARK_MASTER:9000/Q1-Part1/input/
